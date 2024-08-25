a7134fdb774733bac6d10292adb756c3834f67e2
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-08-25 13:45:05 +0100

    Add files via upload

---
+++ question.js
@@ -0,0 +1,58 @@
+//An Array passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stands for?",
+        answer: "hyper text markup language",
+        options: [
+            "hyper text processor",
+            "hyper text markup language",
+            "hyper text multiple language",
+            "hyper tool multi language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "what does CSS stands for?",
+        answer: "cascading style sheet",
+        options: [
+            "common style sheet",
+            "colourfull style sheet",
+            "computer style sheet",
+            "cascading style sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "what does PHP stands for?",
+        answer: "Hypertext preprocessor",
+        options: [
+            "Hypertext preprocessor",
+            "Hypertext programming",
+            "Hypertext preprogrramming",
+            "Hypertext processor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "what does SQL stands for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "what does XML stands for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multi Language",
+            "eXtra Multi-program Language",
+            "eXamine Multi Language"
+        ]
+    },
+];
